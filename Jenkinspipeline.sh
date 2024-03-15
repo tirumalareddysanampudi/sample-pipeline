ec4d248cfe55d288ba2698a71a43ee6aac4495c6
tirumalareddysanampudi <45302057+tirumalareddysanampudi@users.noreply.github.com>
2024-03-15 10:29:40 +0530

    Update Jenkinspipeline.sh

--- Jenkinspipeline.sh
+++ Jenkinspipeline.sh
@@ -1,17 +1,15 @@
-pipeline {
+pipeline{
  agent any
- environment {
-    JAVA_HOME ="/usr/lib/jvm/java-11-openjdk-amd64"
-    PATH ="$JAVA_HOME/bin:$PATH"
-    MAVEN_PATH ="/usr/share/maven"
-    PATH ="$MAVEN_PATH/bin:$PATH"
+ environment{
+   PATH=$JAVA_HOME/bin:$PATH
+   PATH=$MAVEN_PATH/bin:$PATH
+ }
+stages{
+ stage('Git-Clone'){
+    steps{
+        git credentialsId: 'Git-Hub', url: 'https://github.com/tirumalareddysanampudi/sample-pipeline.git'
     }
- stages{
-    stage('Git-Checkout'){
-        steps{
-       git branch: 'tirumala-Branch', credentialsId: 'Git-HUB-CREDENTIALS', url: 'https://github.com/tirumalareddysanampudi/maven_Project.git'
-    }
-        }
-    }
-    }
-    }
+ }
+
+}
+}
